12e57f3efa5549c746ad0876a62e889d54e07aab
The Sewn Identity <179808961+The-Sewn-Identity@users.noreply.github.com>
2025-05-09 22:37:20 +0200

    Music player added

--- script.js
+++ script.js
@@ -28,3 +28,45 @@ function changeColor(theme) {
             document.querySelector('#right').style = "background-color: #50988A"
     }
 }
+
+var mus = document.querySelector('#frame')
+let spot_URLs = [
+    "https://open.spotify.com/embed/track/4oLEixV6gLlHMiecqSZOLY?utm_source=generator", // Sturm & Drang
+    "https://open.spotify.com/embed/track/1mh0UmMFz040KPisigYy7b?utm_source=generator" // Back in the U.S.S.A.
+]
+
+var foot = document.querySelector('footer')
+
+var show_status = false
+
+$(document).ready(function() {
+    $("#music").click(function() {
+        let rand = Math.floor(Math.random() * spot_URLs.length)
+        mus.src = spot_URLs[rand]
+
+        console.log(rand)
+
+        show_status = !show_status
+        switch (show_status) {
+            case true:
+                $("#frame").css("display", "initial");
+                break;
+            case false:
+                $("#frame").css("display", "none");
+                break;
+        }
+    })
+});
+
+function update() {
+    mus.style.left = "calc(50% - " + mus.clientWidth/2 + "px)";
+
+    if (window.innerWidth < 576) {
+        mus.style.bottom = 16 + foot.clientHeight + "px";
+    }
+    else {
+        mus.style.bottom = 'initial';
+    }
+}
+
+setInterval(update, 1)
